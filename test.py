f3384f08c352f35f94ba17752d87b2170c4c64a4
David-Crumps <david.crumplin@gmail.com>
2025-08-26 10:49:29 +0200

    Getting a space invader to move

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import pygame
 import os
 
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+speed = [0, 1]
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, 'Data')
 
 invader = pygame.transform.scale(pygame.image.load(os.path.join(data_dir, 'invader.png')), (80, 80))
-invaderRect = invader.get_rect(center=(400, 400))
+invaderPos = invader.get_rect(center=(640, 360))
 
 
 while running:
@@ -18,11 +20,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         
+    invaderPos = invaderPos.move(speed)
+
+
     screen.fill('black')
-    screen.blit(invader, invaderRect)
-
+    screen.blit(invader, invaderPos)
     pygame.display.flip()
-
     clock.tick(60)
 
 pygame.quit()
+
