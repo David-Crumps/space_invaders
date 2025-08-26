7832a686333d51145bf081a31b4cca57112234df
David-Crumps <david.crumplin@gmail.com>
2025-08-26 10:14:44 +0200

    adding invader images

--- test.py
+++ test.py
@@ -1,16 +1,26 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((2560, 1440))
+screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+
+invader = pygame.transform.scale(pygame.image.load('invader_5.png'), (80, 80))
+invader2 = pygame.transform.scale(pygame.image.load('invader_2.png'), (80, 80))
+invader3 = pygame.transform.scale(pygame.image.load('invader_3.png'), (80, 80))
+invaderRect = invader.get_rect(center=(400, 400))
+invaderRect2 = invader.get_rect(center=(500, 100))
+invaderRect3 = invader.get_rect(center=(640, 360))
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         
-    screen.fill('pink')
+    screen.fill('black')
+    screen.blit(invader, invaderRect)
+    screen.blit(invader2, invaderRect2)
+    screen.blit(invader3, invaderRect3)
 
     pygame.display.flip()
 
