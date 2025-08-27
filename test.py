d4d1f0c6448844b8f2aaf2e889c5d688a8f51e0e
David-Crumps <david.crumplin@gmail.com>
2025-08-27 10:12:16 +0200

    adding new stuff
    learning how to add player movement and limit objects so they don't go off screen, added a bullet that shoots from the location of the ship and limiting how often it shoots.

--- test.py
+++ test.py
@@ -3,11 +3,15 @@ import os
 
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+size = width, height = 1280, 720
+screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 running = True
-speed = [0, 1]
-playerSpeed = [4, 0]
+invader_speed = 80
+player_speed = 450
+bullet_speed = 600
+
+dt = 0
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, 'Data')
@@ -19,20 +23,58 @@ player = pygame.transform.scale(pygame.image.load(os.path.join(data_dir, 'player
 playerPos = player.get_rect(center=(640, 670))
 
 
+bullets = []
+
+bullet_img = pygame.transform.scale(pygame.image.load(os.path.join(data_dir, 'bullet.png')), (30,30))
+
+bullet_cooldown = 250 #milliseconds
+last_shot_time = 0
+
+
 
 while running:
+    dt = clock.tick(60)/ 1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
-    invaderPos = invaderPos.move(speed)
+  
+    #Limit bullets shot:
+    current_time = pygame.time.get_ticks()
+    keys = pygame.key.get_pressed() 
+    if keys[pygame.K_SPACE] and current_time - last_shot_time > bullet_cooldown:
+        bullet_rect = bullet_img.get_rect()
+        bullet_rect.midbottom = playerPos.midtop
+        bullets.append((bullet_img, bullet_rect))
+        last_shot_time = current_time
 
+
+
+    player_x = 0    #moving left and right
+    if keys[pygame.K_LEFT]:
+        player_x = -player_speed
+    if keys[pygame.K_RIGHT]:
+        player_x = player_speed
+    playerPos = playerPos.move(player_x*dt, 0)
+
+    if playerPos.left < 0: #checking collision with screen
+        playerPos.left = 0
+    if playerPos.right > width:
+        playerPos.right = width
+
+
+
+    invaderPos = invaderPos.move(0, invader_speed*dt)
 
     screen.fill('black')
     screen.blit(invader, invaderPos)
     screen.blit(player, playerPos)
+
+    for bullet_img, bullet_rect in bullets:
+        bullet_rect.move_ip(0, -bullet_speed * dt)
+        screen.blit(bullet_img, bullet_rect)
+
     pygame.display.flip()
-    clock.tick(60)
 
 pygame.quit()
 
