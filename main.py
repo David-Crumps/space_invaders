3f62158539a0d00a4fb53d1cc6e1b1e80385723e
David-Crumps <david.crumplin@gmail.com>
2025-08-28 10:51:50 +0200

    Testing
    just testing how things are working

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 import os
 from player import Player
+from invader import Invader
+from sprite_pool import SpritePool
+from InvaderSpawnManager import InvaderSpawnManager
 import sys
 import json
+import random
 
 
 
@@ -29,6 +33,11 @@ def main():
 
     player = Player(player_start_pos, player_img, player_speed)
 
+    invader_img = pygame.transform.scale(pygame.image.load(os.path.join("Data", "invader.png")), (80,80))
+
+    invader_group = SpritePool(cls=Invader, size = 12, spawn_strategy=InvaderSpawnManager(), image=invader_img, speed=100)
+    invader_group.spawn_all()   
+
     del player_img, player_speed, player_start_pos
     while True:
         dt = clock.tick(60) / 1000 #Delta time
@@ -38,9 +47,12 @@ def main():
         
         keys = pygame.key.get_pressed() 
         player.update(dt, keys, width)
-
+        invader_group.update(dt, height)
+        
         screen.fill('black')
         screen.blit(player.image, player.rect)
+        invader_group.draw(screen)
+        
         
         pygame.display.flip()
 
