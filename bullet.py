a9f4f25ffc2e2bb0ab227972831d72a7d5ba4d7f
David-Crumps <david.crumplin@gmail.com>
2025-08-28 11:32:36 +0200

    Bullet class

---
+++ bullet.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, image, speed):
+        super().__init__()
+        self.image = image
+        self.rect = image.get_rect()
+        self.speed = speed
+        self.damage = 1
+        self.active = True
+    
+    def update(self, dt):
+        self.rect.move_ip(0, -self.speed*dt)
+  
+        if self.rect.top < 0:
+            self.active = False
+
+def bullet_spawn_strat(bullet, player):
+    bullet.rect.midbottom = player.rect.midtop
