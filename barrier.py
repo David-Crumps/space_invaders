22f624a763f36688c84a1e2bf8b48c088b7358fa
David-Crumps <david.crumplin@gmail.com>
2025-09-01 11:09:03 +0200

    Created barrier class, to block a number of invaders before breaking

--- barrier.py
+++ barrier.py
@@ -1,16 +1,18 @@
 import pygame
 
 class Barrier(pygame.sprite.Sprite):
+    damage = 50
     def __init__(self, image):
         super().__init__()
         self.image = image
         self.active = False
         self.rect = self.image.get_rect()
         self.health = 2
+        
 
     def update(self):
         if self.health <= 0:
             self.active = False
     
-    def collision(self):
+    def collision(self, **kwargs):
         self.health -= 1
